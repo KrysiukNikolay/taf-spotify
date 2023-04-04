8fbe4f455396f6ed0909d397b13d44d32d2ae0a3
KrysiukNikolay <Kolian888@yandex.ru>
2023-04-05 00:10:23 +0300

    install allure

--- src/test/java/spotify/api/token/GetToken.java
+++ src/test/java/spotify/api/token/GetToken.java
@@ -2,5 +2,5 @@ package spotify.api.token;
 
 public class GetToken {
 
-    public static final String ACCESS_TOKEN ="";
+    public static final String ACCESS_TOKEN ="BQDJNrajd9hk4qKR6kzUQfDZZ_YviIo_j5lTDravxfYyVWZjb2IQs9eagaCCju_yGNodT-p3fCVRabaYG8mq6AyEoBOFqpNWN0D06KmaKIJx0tq68JUyQ1CGGfUYSz1YZN3GuVZqKUqbABpMzvGT7ABZ7WlDA-0Edqlmrv21iUgbedVOCnPGkRFbKg9hbM7zbeOjYRHQT5_lD-bBgMWOY77B8YarnspWWmtYxFm-5fCe1NYVLulXIrNDYs-dG9zoatTsVCsVwkOB630";
 }
