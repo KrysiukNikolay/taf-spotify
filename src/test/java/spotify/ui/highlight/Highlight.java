326c3a22a6730b8f77ad8bacceab84117d780a19
KrysiukNikolay <Kolian888@yandex.ru>
2023-04-10 18:41:58 +0300

    add Highlight Web Elements

---
+++ src/test/java/spotify/ui/highlight/Highlight.java
@@ -0,0 +1,14 @@
+package spotify.ui.highlight;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import spotify.ui.pages.BasePage;
+
+public class Highlight extends BasePage {
+
+    public static void highlight(WebElement element) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].setAttribute('style', 'border: 6px solid red; background:yellow');", element);
+        js.executeScript("arguments[0].setAttribute('style', 'border: none; background:yellow');", element);
+    }
+}
