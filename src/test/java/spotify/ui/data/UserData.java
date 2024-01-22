843aa8300677ab3f1faeeb8f027816a79aa92cac
KrysiukNikolay <122222877+KrysiukNikolay@users.noreply.github.com>
2024-01-22 11:10:36 +0300

    Update UserData.java
    edit pass

--- src/test/java/spotify/ui/data/UserData.java
+++ src/test/java/spotify/ui/data/UserData.java
@@ -7,7 +7,7 @@ public class UserData {
     static Faker faker = new Faker();
 
     public static final String USER_EMAIL = "krysiuk.nikolay@gmail.com";
-    public static final String USER_PASSWORD = "Spotify2013";
+    public static final String USER_PASSWORD = "Spo***2013";
     public static final String EMPTY_USER_EMAIL = "";
     public static final String EMPTU_USER_PASSWORD = "";
     public static final String RANDOM_USER_EMAIL = faker.internet().emailAddress();
